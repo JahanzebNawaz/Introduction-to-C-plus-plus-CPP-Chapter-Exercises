8cdb887936865beecd4bd295d7ee7ba1d4627935
Jahanzaib Nawaz <34652154+JahanzebNawaz@users.noreply.github.com>
2019-01-22 21:54:43 +0500

    Write a program  to find out the area of triangle when three sides

--- chapter-1/Q3.cpp
+++ chapter-1/Q3.cpp
@@ -3,7 +3,7 @@ sides a, b and c of the triangle are given. use appropriate statements
 to input values a,b and c from the keyboard. Formula for the area of 
 triangle is as follow 
 
-Area = underroot s(s-a) (s-b) (s-c) 
+Area = underroot s(s-a) (s-b) (s-c).
 
 Where s=(a+b+c)/2.  */
 
