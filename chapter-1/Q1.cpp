283d51dd8d26795d44eb6c2286ac410f9b92bebb
jahanzebnawaz <jahanzaib.ch9996@gmail.com>
2019-01-22 21:44:56 +0500

    started solving exercises of Object Oriented Programming in C++

---
+++ chapter-1/Q1.cpp
@@ -0,0 +1,20 @@
+/* Q1. write a program that prints a text of 4 lines consisting of characters
+intergers values and floating point values using cout statement. */
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+main()
+{
+	int mark=789;
+	float avg= 789.09;
+	char grade= 'A';
+	system("cls");
+	cout<<"I am Hammad Khan "<<endl;
+	cout<<"My Total marks are "<<mark<<endl;
+	cout<<"Average of Marks "<<avg<<endl;
+	cout<<"Grades Of Result "<<grade;
+	getch();
+	
+}
